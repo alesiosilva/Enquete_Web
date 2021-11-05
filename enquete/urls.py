5c4ad0d75878f30021fd2ae0767c2911b6c12a65
Alesio Torres <alesiosilva@gmail.com>
2021-11-04 21:29:28 -0300

    Organização de urls.py

--- enquete/urls.py
+++ enquete/urls.py
@@ -1,4 +1,3 @@
-from os import name
 from django.urls import path
 from . import views
 
@@ -6,9 +5,9 @@ from . import views
 app_name = 'enquete'
 urlpatterns = [
     path('', views.index, name='index'),
-    # Substituindo views.<name_template> --> views.NameView.as_view()
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
     path('contato/', views.contact, name='contact')
+    # Substituindo views.<name_template> --> views.NameView.as_view()
 ]
