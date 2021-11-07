d865ac60e0a23af9ba27084323e1378fb88ce5d3
Alesio Torres <alesiosilva@gmail.com>
2021-11-07 11:37:36 -0300

    View contact atualizada para renderizar apenas form sem email

--- curso/views.py
+++ curso/views.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.core.mail import send_mail
-from django.forms import ModelForm
 from django.shortcuts import get_object_or_404, render, redirect
 
 from .models import *
+from .forms import *
 
 # Create your views here.
 
@@ -16,25 +16,39 @@ def detail(request, slug):
     curso = get_object_or_404(Course, slug=slug)
     return render(request, 'curso/detail.html', {'curso': curso})
 
-class ContactForm(ModelForm):
-    class Meta:
-        model = Contact
-        fields = '__all__'
-
+# Nova view de contato com form e envio de email separados
 def contact(request):
-    contato = ContactForm(request.POST or None)
-    course = Course.objects.get(pk=request.POST['course'])
-    if contato.is_valid():
-        contato.save()
-        name = contato.cleaned_data['name']
-        email = contato.cleaned_data['email']
-        msg = contato.cleaned_data['msg']
-        date = contato.cleaned_data['date']
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        course = Course.objects.get(pk=request.POST['course'])
+        form.send_mail(course)
+        form.save()
+        form = ContactForm()
+        alert = 'Obrigado, recebemos sua mensagem com sucesso!'
+        context = {
+            'form': form,
+            'alert': alert,
+        }
+        return render(request, 'curso/contact.html', context)
+    return render(request, 'curso/contact.html', {'form': form})
+        
+    
+
+''' Antigo contato com envio de e-mail integrado a view
+def contact(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        course = Course.objects.get(pk=request.POST['course'])
+        form.save()
+        name = form.cleaned_data['name']
+        email = form.cleaned_data['email']
+        msg = form.cleaned_data['msg']
         subject = 'Contato sobre o curso: ' + course.name
         message = 'Olá prezado, %(name)s\nRecebemos sua solicitação através do nosso formulário Django: \nE-mail: %(email)s\nMensagem: %(msg)s \nData: %(date)s'
-        context = {'name': name, 'email': email, 'msg': msg, 'date': date,}
+        context = {'course': course.name, 'name': name, 'email': email, 'msg': msg,}
         message = message % context
         alert = 'Obrigado, recebemos sua mensagem com sucesso!'
         send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [email])
-        return render(request, 'curso/contact.html', {'contato': contato, 'alert': alert})
-    return render(request, 'curso/contact.html', {'contato': contato})
+        return render(request, 'curso/contact.html', {'form': form, 'alert': alert})
+    return render(request, 'curso/contact.html', {'form': form})
+'''
