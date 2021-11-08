cf9942609a75b40ba1049640722353e7b97e74a7
Alesio Torres <alesiosilva@gmail.com>
2021-11-07 21:11:14 -0300

    Login automático após cadastro usando authenticate e login de contrib.auth

--- user/views.py
+++ user/views.py
@@ -1,25 +1,29 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render, get_object_or_404
-from django.contrib.auth.views import LoginView
 #from django.contrib.auth.forms import UserCreationForm
 from .forms import RegisterForm
 from django.conf import settings
 
 # Create your views here.
 
-def login(request):
-    login = LoginView()
-    return render(request, login)
-
 # View para cadastro de usuários no django
 def register(request):
     template_name = 'user/register.html' 
     form = RegisterForm(request.POST or None)
     if form.is_valid():
-        form.save()
-        return redirect(settings.LOGIN_URL)
+        user = form.save()
+
+        # Login automático após cadastro
+        user = authenticate(
+            username = user.username,
+            password = form.cleaned_data['password1'],
+        )
+        login(request, user)
+        # Retorno para index após cadastro
+        return redirect('curso:index')
+        
+        # Retorno para tela de URL após cadastro
+        # return redirect(settings.LOGIN_URL)
     context = {'form': form}
     return render(request, template_name, context)
-    
-def logout(request):
-    return render(request, 'user/logout.html')
     
