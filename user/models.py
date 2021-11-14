406c7f1ce192a470207ca25523d0e406556b0ea4
Alesio Torres <alesiosilva@gmail.com>
2021-11-14 18:42:34 -0300

    Restauração de senha completa para app user com URLs e Forms de PasswordResetViews + templates de reset, reset_done, reset_confirme e reset_complete

--- user/models.py
+++ user/models.py
@@ -4,7 +4,11 @@ from django.contrib.auth.models import User, AbstractUser
 from django.db.models.deletion import CASCADE
 
 # Model customizado para recuperação de senha
-
+'''
 class PasswordReset(models.Model):
     user = models.ForeignKey(User, on_delete=CASCADE, verbose_name='Usuário')
+    key = models.CharField('Chave', max_length=100, unique=True)
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
 
+    def __str__(self) -> str:
+        return super().__str__()'''
--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, base
 from django.urls.conf import include
 from . import views
 from django.contrib.auth import views as auth_views
@@ -7,10 +7,19 @@ app_name = 'user'
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('register/', views.register, name='register'),
-    path('logout/', auth_views.LogoutView.as_view(next_page = 'curso:index'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='curso:index'), name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('edit_user/', views.edit_user, name='edit_user'),
     path('edit_password/', views.edit_password, name='edit_password'),
-    path('recovery/', auth_views.PasswordResetView.as_view(), name='recovery')
-
+    path('reset/', auth_views.PasswordResetView.as_view(
+        template_name='user/reset.html',
+        email_template_name='user/reset_email.html',
+        success_url=base.reverse_lazy('user:password_reset_done')), name='password_reset'),
+    path('reset/done', auth_views.PasswordResetDoneView.as_view(
+        template_name='user/reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(
+        template_name='user/reset_confirm.html', 
+        success_url=base.reverse_lazy('user:password_reset_complete')), name='password_reset_confirm'),
+    path('reset_complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='user/reset_complete.html'), name='password_reset_complete'),
 ]
